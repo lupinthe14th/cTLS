f42dcd7b077c5f316e8e8e1cb061dd476c85e15b
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-06-26 06:49:03 +0900

    :tada: Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lupinthe14th/cTLS
+
+go 1.12
+
+require github.com/sirupsen/logrus v1.4.2
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
+}
+
+func main() {
+	conn, err := tls.Dial("tcp", "cloud.ordinarius-fectum.net:443", &tls.Config{})
+	if err != nil {
+		log.Fatalf("client: dial: %s", err)
+	}
+	defer conn.Close()
+	log.Println("client: connected to: ", conn.RemoteAddr())
+
+	state := conn.ConnectionState()
+	for _, v := range state.PeerCertificates {
+		if !v.IsCA {
+			log.Println(v.Subject)
+			expireUTCTime := v.NotAfter
+			expireJSTTime := expireUTCTime.In(time.FixedZone("Asia/Tokyo", 9*60*60))
+			fmt.Println("Peer Certificates: expire date: ", expireJSTTime)
+		}
+	}
+	log.Print("client: exiting")
+}
