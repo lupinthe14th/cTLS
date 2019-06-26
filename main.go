8b60eb9818012a45b561d2778780d52d3ff11d13
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-06-27 05:57:55 +0900

    :recycle: Refactoring

--- main.go
+++ main.go
@@ -13,22 +13,32 @@ func init() {
 	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 }
 
-func main() {
-	conn, err := tls.Dial("tcp", "www.google.com:443", &tls.Config{})
+func statePeerCertificateExpireDate(host string, port uint32) (expireTime time.Time, err error) {
+	conn, err := tls.Dial("tcp", fmt.Sprint(host, ":", port), &tls.Config{})
 	if err != nil {
-		log.Fatalf("client: dial: %s", err)
+		log.Errorf("client: dial: %s", err)
+		return expireTime, err
 	}
 	defer conn.Close()
-	log.Println("client: connected to: ", conn.RemoteAddr())
+	log.Debugln("client: connected to: ", conn.RemoteAddr())
 
 	state := conn.ConnectionState()
 	for _, v := range state.PeerCertificates {
 		if !v.IsCA {
 			log.Println(v.Subject)
-			expireUTCTime := v.NotAfter
-			expireJSTTime := expireUTCTime.In(time.FixedZone("Asia/Tokyo", 9*60*60))
-			fmt.Println("Peer Certificates: expire date: ", expireJSTTime)
+			expireTime = v.NotAfter
+			log.Println("peer certificates: expire time: ", expireTime)
 		}
 	}
-	log.Print("client: exiting")
+	log.Debugln("client: exiting")
+	return expireTime, nil
+}
+
+func main() {
+	expireTime, err := statePeerCertificateExpireDate("www.google.com", 443)
+	if err != nil {
+		log.Panicln(err)
+	}
+	expireJSTTime := expireTime.In(time.FixedZone("Asia/Tokyo", 9*60*60))
+	fmt.Println("Peer Certificates: expire time: ", expireJSTTime)
 }
