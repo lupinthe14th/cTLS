829b6c685bc26cad26c0dd32158f91a5ab0f6461
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-07-02 00:00:45 +0900

    :rotating_light: add testcase

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestStatePeerCertificateExpireDate(t *testing.T) {
+	var tests = []struct {
+		host string
+		port string
+	}{
+		{host: "www.google.com", port: "443"},
+	}
+
+	for _, tt := range tests {
+		expireTime, err := statePeerCertificateExpireDate(tt.host, tt.port)
+		if err != nil {
+			t.Error(err)
+		}
+		expireJSTTime := expireTime.In(time.FixedZone("Asia/Tokyo", 9*60*60))
+		t.Logf("Peer Certificates: expire time: %+v", expireJSTTime)
+	}
+}
